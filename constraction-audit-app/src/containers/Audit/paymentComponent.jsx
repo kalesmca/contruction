f7e176f01f30cdcd8821b68cacb7e2cee758ec4e
kaleeswaran <kalesmca@gmail.com>
2023-09-10 22:28:26 +0530

    date basis filter done

--- constraction-audit-app/src/containers/Audit/paymentComponent.jsx
+++ constraction-audit-app/src/containers/Audit/paymentComponent.jsx
@@ -1,30 +1,81 @@
-import React,{useState, useEffect} from "react";
-import {useSelector, useDispatch} from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import Table from 'react-bootstrap/Table';
 import { INIT_TOTAL_OBJ } from "../../config/constants";
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
-
-
-
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { addDays } from 'date-fns';
+import { DateRange } from 'react-date-range';
+import {formatAppDate} from '../../config/utils';
 // import {}
 
-const PaymentComponent = () =>{
+const PaymentComponent = () => {
     const dispatch = useDispatch();
-    const appState = useSelector((state)=>state);
+    const appState = useSelector((state) => state);
     const [list, setList] = useState([]);
+    const [isDateFilter, setDateFilterFlag] = useState(false)
     const [totalObj, setTotalObj] = useState(INIT_TOTAL_OBJ);
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+        }
+    ]);
     useEffect(()=>{
+        filterByDate()
+    },[date])
+    useEffect(() => {
         setList(appState?.entry?.entryList)
-    },[appState])
-    console.log('list:', list)
-    const onSelectRow = (index) =>{
+    }, [appState])
+    console.log('list:', list, date)
+    const onSelectRow = (index, e) => {
+        console.log(e)
         let tempList = list;
-        tempList[index].isSelected = !tempList[index].isSelected;
+        tempList[index].isSelected = e.target.checked;
         setList([...tempList])
     }
-    return(
+    const filterByDate = ()=>{
+        let filteredList = appState?.entry?.entryList.filter((data)=> {
+            let entryDate= new Date(data.date)
+            if(entryDate>=date[0].startDate && entryDate<= date[0].endDate)
+            {
+                return data;
+            }
+             
+        })
+        console.log('filterlist :', filteredList)
+        setList([...filteredList])
+
+    }
+
+
+    return (
         <div>
+            <div>
+                <InputGroup className="mb-3">
+                    <InputGroup.Checkbox aria-label="Checkbox for following text input" checked={isDateFilter}
+                        onChange={(e) => { setDateFilterFlag(e.target.checked) }}
+                    /> 
+                    <Form.Control aria-label="Text input with checkbox" placeholder="Date Filter" disabled={true} value={formatAppDate(date[0]?.startDate) +" -TO- " + date[0]?.endDate }/>
+
+                </InputGroup>
+            </div>
+            {
+                isDateFilter && (
+                    <div>
+                <DateRange
+                    editableDateInputs={true}
+                    onChange={item => setDate([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}
+                />
+            </div>
+                )
+            }
+            
             <div>
                 <Table responsive="sm">
                     <thead>
@@ -48,7 +99,8 @@ const PaymentComponent = () =>{
                                         <td>
                                             <InputGroup className="mb-3">
                                                 <InputGroup.Checkbox aria-label="Checkbox for following text input" checked={entry.isSelected}
-                                                onClick={()=>{onSelectRow(entryIndex)}}
+                                                    // onClick={()=>{onSelectRow(entryIndex)}}
+                                                    onChange={(e) => { onSelectRow(entryIndex, e) }}
                                                 />
                                             </InputGroup>
                                         </td>
