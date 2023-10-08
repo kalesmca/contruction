25c7def38ab50a42278fc205e4965f0ead304640
kaleeswaran <kalesmca@gmail.com>
2023-10-08 12:43:47 +0530

    till data

--- constraction-audit-app/src/containers/dashboard/dashboardComponent.jsx
+++ constraction-audit-app/src/containers/dashboard/dashboardComponent.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import {useSelector, useDispatch} from 'react-redux';
-import "./dashboard.scss"
+import "./dashboard.scss";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
 
 const DashboardComponent =() =>{
+    const appState = useSelector(state => state)
+    console.log('all : ', appState.entry.entryList, appState.appConfig.configList)
     return (
         <div>
             Dashboard 
+            <div>Entry - List</div>
+            <div>
+                {JSON.stringify(appState.entry.entryList)}
+            </div>
+            <div>
+                -----------------------------------------------------------------------------------------
+            </div>
+            <div>
+            {JSON.stringify(appState.appConfig.configList)}
+            </div>
         </div>
     )
 }
